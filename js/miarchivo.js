fdfe48e530f193b2dd8eb28c79ca9783e0957690
lulceles <lucianaalonso98@gmail.com>
2022-07-21 13:04:10 -0300

    commit

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,37 +1,3 @@
-// // let nombreIngresado = prompt("Bienvenido/a! ¿Como es tu nombre? Si no desea brindar esa información, escribir ESC");
-// // while (nombreIngresado !== "ESC"){
-// //     let saludo  = "¡Hola, " + nombreIngresado + "!";
-// //     alert(saludo);
-// //     break;
-// // }
-
-
-// // Objetos
-// class Torta {
-//     constructor( nombre, sabor, precio ){
-//         this.nombre = nombre;
-//         this.sabor = sabor;
-//         this.precio = parseFloat(precio);
-//     }
-//     mostrarProducto() {
-//         return (
-//         " Precio: " +
-//         this.precio + 
-//         " " +
-//         " Producto: " +
-//         this.nombre +
-//         "\n"
-//         );
-//     }
-//     sumaIva() {
-//         alert("El precio con IVA es de $" + this.precio * 1.21);
-//     }
-
-// }
-
-// const torta1 = new Torta( "Chocotorta" , "Dulce de leche y Chocolinas", "1200");
-// const torta2 = new Torta("Clasica", "Vainilla o Chocolate", "1000");
-// const torta3 = new Torta("Clasica con forma de inicial", "Brownie", "1500");
 
 // do{
 //     elegirTorta = parseInt(prompt(`Pedir precio de la torta: (ingresar número) \n\n 1- ${torta1.nombre}, sabor ${torta1.sabor} \n 2- ${torta2.nombre},  sabores ${torta2.sabor}\n 3- ${torta3.nombre}, sabor ${torta3.sabor} \n 4- Salir`))
@@ -50,12 +16,6 @@
 //             break;
 //     }
 // } while(elegirTorta !== 4 );
-
-
-
-
-
-
 
 
 // // CARRITO
@@ -93,87 +53,129 @@
 
 
 
+// OBJETOS TORTAS
+class Torta {
+    constructor( nombre, sabor, precio ){
+        this.nombre = nombre;
+        this.sabor = sabor;
+        this.precio = parseFloat(precio);
+    }
+    mostrarProducto() {
+        return (
+        " Precio: " +
+        this.precio + 
+        " " +
+        " Producto: " +
+        this.nombre +
+        "\n"
+        );
+    }
+    sumaIva() {
+        alert("El precio con IVA es de $" + this.precio * 1.21);
+    }
+
+}
+
+const torta1 = new Torta( "Chocotorta" , "Dulce de leche y Chocolinas", "1200");
+const torta2 = new Torta("Clasica", "Vainilla o Chocolate", "1000");
+const torta3 = new Torta("Clasica con forma de inicial", "Brownie", "1500");
 
 
 
 
 
-// // NEWSLETTER
 
-// let inputNombre;
-// let inputMail;
-// let boton;
-// let salidaNews;
-// boton = document.getElementById("btnNews");
-// inputNombre = document.querySelector(".newsletterFormNombre");
-// inputMail = document.querySelector(".newsletterForm");
-// salidaNews = document.getElementById("Newsletter");
+// NEWSLETTER
 
-// inputNombre.oninput = () => {
-//     nombreForm = inputNombre.value;
-// }
-// inputMail.oninput = () => {
-//     emailForm = inputMail.value;
-// }
-// boton.onclick = (event) => {
-//     event.preventDefault();
-//     let salidaNewsletter = "Gracias " + nombreForm + " por brindarnos tu mail: " + emailForm;
-//     if(inputMail.value != ""){
-//         salidaNews.innerHTML = `
-//         ${salidaNewsletter}
-//         `
+let inputNombre;
+let inputMail;
+let boton;
+let salidaNews;
+boton = document.getElementById("btnNews");
+inputNombre = document.querySelector(".newsletterFormNombre");
+inputMail = document.querySelector(".newsletterForm");
+salidaNews = document.getElementById("Newsletter");
 
-//         errores.style.display = "none";
-//     }else{
-//         errores.style.display = "block"
-//     }  
-// } 
-
-
-
-// // GUSTOS 
-
-// let gustos = [torta1.sabor, torta2.sabor, torta3.sabor];
-
-// let formularioGustos = document.getElementById("formularioGustos");
-// let tabla = document.getElementById("tablaGustos");
-// let errores = document.querySelector(".errores")
-// errores.style.display = "none";
-// let inputGusto = document.getElementById("gusto");
+inputNombre.oninput = () => {
+    nombreForm = inputNombre.value;
+}
+inputMail.oninput = () => {
+    emailForm = inputMail.value;
+}
+boton.onclick = (event) => {
+    event.preventDefault();
+    let salidaNewsletter = "Gracias " + nombreForm + " por brindarnos tu mail: " + emailForm;
+    if(inputMail.value != ""){
+        salidaNews.innerHTML = `
+        ${salidaNewsletter}
+        `
+        errores.style.display = "none";
+    }else{
+        errores.style.display = "block"
+    }  
+} 
 
 
-// formularioGustos.onsubmit = (event) => {
-//     event.preventDefault();
-//     let nuevoGusto = inputGusto.value;
-//     if(inputGusto.value != ""){
-//         gustos.push(nuevoGusto);
-//         limpiarTabla();
-//         agregarGustosTabla();
-//         errores.style.display = "none";
-//         formularioGustos.reset();
-//     }else{
-//         errores.style.display = "block"
-//     }    
-// }
 
-// function limpiarTabla(){
-//     while(tabla.rows.length > 1){
-//         tabla.deleteRow(1)
-//     }
-// }
+// GUSTOS 
 
-// function agregarGustosTabla(){
-//     gustos.forEach(gustos => {
-//         let tabla = document.querySelector(".tabla")
-//         let filaTabla = document.createElement("tr")
+let gustos = [torta1.sabor, torta2.sabor, torta3.sabor];
 
-//         filaTabla.innerHTML = `
-//         <td> ${gustos} </td>
-//         `
+let formularioGustos = document.getElementById("formularioGustos");
+let tabla = document.getElementById("tablaGustos");
+let errores = document.querySelector(".errores")
+errores.style.display = "none";
+let inputGusto = document.getElementById("gusto");
 
-//     tabla.append(filaTabla)
 
-//     });
-// }
+formularioGustos.onsubmit = (event) => {
+    event.preventDefault();
+    let nuevoGusto = inputGusto.value;
+    if(inputGusto.value != ""){
+        gustos.push(nuevoGusto);
+        limpiarTabla();
+        agregarGustosTabla();
+        errores.style.display = "none";
+        formularioGustos.reset();
+    }else{
+        errores.style.display = "block"
+    }    
+}
 
+function limpiarTabla(){
+    while(tabla.rows.length > 1){
+        tabla.deleteRow(1)
+    }
+}
+
+function agregarGustosTabla(){
+    gustos.forEach(gustos => {
+        let tabla = document.querySelector(".tabla")
+        let filaTabla = document.createElement("tr")
+
+        filaTabla.innerHTML = `
+        <td> ${gustos} </td>
+        `
+
+    tabla.append(filaTabla)
+
+    });
+}
+
+// CONSULTA
+
+let formularioConsulta = document.getElementById("formulario")
+let consultaMail = document.getElementById("exampleInputEmail1")
+let consultaNombre = document.getElementById("inputNombre");
+
+formularioConsulta.onsubmit = (event) => {
+    event.preventDefault();
+    let nombreContacto = consultaNombre.value;
+    if(consultaMail.value != ""){
+        formularioConsulta.innerHTML = `
+        Gracias por contactarnos, ${nombreContacto}, a la brevedad respondemos!`}
+        else{
+        errores.style.display = "block"}
+
+    }
 
