2ce8fe433921416e2513226bdaf0c2492b8d0e01
lulceles <lucianaalonso98@gmail.com>
2022-08-09 12:14:57 -0300

    commit

--- js/main.js
+++ js/main.js
@@ -1,78 +1,71 @@
-
 // OBJETOS TORTAS
 class Torta {
-    constructor( nombre, sabor, precio ){
-        this.nombre = nombre;
-        this.sabor = sabor;
-        this.precio = parseFloat(precio);
-    }
-
+  constructor(nombre, sabor, precio) {
+    this.nombre = nombre;
+    this.sabor = sabor;
+    this.precio = parseFloat(precio);
+  }
 }
 
-const torta1 = new Torta( "Chocotorta" , "Dulce de leche y Chocolinas", "1200");
+const torta1 = new Torta("Chocotorta", "Dulce de leche y Chocolinas", "1200");
 const torta2 = new Torta("Clasica", "Vainilla o Chocolate", "1000");
 const torta3 = new Torta("Clasica con forma de inicial", "Brownie", "1500");
 
-
-// GUSTOS 
+// GUSTOS
 
 let gustos = [torta1.sabor, torta2.sabor, torta3.sabor];
 
 let formularioGustos = document.getElementById("formularioGustos");
 let tabla = document.getElementById("tablaGustos");
-let errores = document.getElementById("errores")
+let errores = document.getElementById("errores");
 errores.style.display = "none";
 let inputGusto = document.getElementById("gusto");
 
-function tablaInicial(){
-    gustos.forEach(gustos => {
-        let tabla = document.querySelector(".tabla")
-        let filaTabla = document.createElement("tr")
+function tablaInicial() {
+  gustos.forEach((gustos) => {
+    let tabla = document.querySelector(".tabla");
+    let filaTabla = document.createElement("tr");
 
-        filaTabla.innerHTML = `
+    filaTabla.innerHTML = `
         <td> ${gustos} </td>
-        `
-    tabla.append(filaTabla)
-    })
+        `;
+    tabla.append(filaTabla);
+  });
 }
-tablaInicial()
+tablaInicial();
 
 formularioGustos.onsubmit = (event) => {
-    event.preventDefault();
-    let nuevoGusto = inputGusto.value;
-    if(inputGusto.value != ""){
-        gustos.push(nuevoGusto);
-        limpiarTabla();
-        agregarGustosTabla();
-        errores.style.display = "none";
-        formularioGustos.reset();
-    
-    }else{
-        errores.style.display = "block";
-    }    
+  event.preventDefault();
+  let nuevoGusto = inputGusto.value;
+  if (inputGusto.value != "") {
+    gustos.push(nuevoGusto);
+    limpiarTabla();
+    agregarGustosTabla();
+    errores.style.display = "none";
+    formularioGustos.reset();
+  } else {
+    errores.style.display = "block";
+  }
+};
+
+function limpiarTabla() {
+  while (tabla.rows.length > 1) {
+    tabla.deleteRow(1);
+  }
 }
 
-function limpiarTabla(){
-    while(tabla.rows.length > 1){
-        tabla.deleteRow(1)
-    }
-}
+function agregarGustosTabla() {
+  gustos.forEach((gustos) => {
+    let tabla = document.querySelector(".tabla");
+    let filaTabla = document.createElement("tr");
 
-function agregarGustosTabla(){
-    gustos.forEach(gustos => {
-        let tabla = document.querySelector(".tabla")
-        let filaTabla = document.createElement("tr")
-
-        filaTabla.innerHTML = `
+    filaTabla.innerHTML = `
         <td> ${gustos} </td>
-        `
+        `;
 
-    tabla.append(filaTabla)
-
-    });
+    tabla.append(filaTabla);
+  });
 }
-
-
 
 // NEWSLETTER
 
@@ -86,36 +79,33 @@ inputMail = document.querySelector(".newsletterForm");
 salidaNews = document.getElementById("Newsletter");
 
 inputNombre.oninput = () => {
-    nombreForm = inputNombre.value;
-}
+  nombreForm = inputNombre.value;
+};
 inputMail.oninput = () => {
-    emailForm = inputMail.value;
-}
+  emailForm = inputMail.value;
+};
 boton.onclick = (event) => {
-    event.preventDefault();
-    let salidaNewsletter = "Gracias " + nombreForm + " por brindarnos tu mail: " + emailForm;
-    if(inputMail.value != ""){
-        salidaNews.innerHTML = `
+  event.preventDefault();
+  let salidaNewsletter =
+    "Gracias " + nombreForm + " por brindarnos tu mail: " + emailForm;
+  if (inputMail.value != "") {
+    salidaNews.innerHTML = `
         ${salidaNewsletter}
-        `
-    } 
-} 
-
-
+        `;
+  }
+};
 
 // CONSULTA
 
-let formularioConsulta = document.getElementById("formulario")
-let consultaMail = document.getElementById("exampleInputEmail1")
+let formularioConsulta = document.getElementById("formulario");
+let consultaMail = document.getElementById("exampleInputEmail1");
 let consultaNombre = document.getElementById("inputNombre");
 
 formularioConsulta.onsubmit = (event) => {
-    event.preventDefault();
-    let nombreContacto = consultaNombre.value;
-    if(consultaMail.value != ""){
-        formularioConsulta.innerHTML = `
-        Gracias por contactarnos, ${nombreContacto}, a la brevedad respondemos!`
-    }
-
-    }
-
+  event.preventDefault();
+  let nombreContacto = consultaNombre.value;
+  if (consultaMail.value != "") {
+    formularioConsulta.innerHTML = `
+        Gracias por contactarnos, ${nombreContacto}, a la brevedad respondemos!`;
+  }
+};
--- js/shop.js
+++ js/shop.js
@@ -1,4 +1,3 @@
-
 // SHOP
 const productsListEl = document.querySelector(".products-list");
 const productEl = document.querySelector(".products");
@@ -6,11 +5,11 @@ const cartItemsEl = document.querySelector(".cart-items");
 const subtotalEl = document.querySelector(".subtotal");
 
 function renderProducts() {
-    fetch('../data.json')
-      .then((resinicial) => resinicial.json())
-      .then((res) => {
-        res.forEach( (product) => {
-            productEl.innerHTML += `
+  fetch("../data.json")
+    .then((resinicial) => resinicial.json())
+    .then((res) => {
+      res.forEach((product) => {
+        productEl.innerHTML += `
             <div class="item">
                 <h4>
                 ${product.name}
@@ -21,68 +20,66 @@ function renderProducts() {
                 <img class="item-img" src=${product.img} alt="${product.name}">
                 <button onclick="addToCart(${product.id})" type="button" class="btn add-to-cart btn-outline-primary">Agregar al pedido</button>
             </div>
-            `
-      })
-    })
-    }
-    renderProducts();
-    
+            `;
+      });
+    });
+}
+renderProducts();
+
 let cart = JSON.parse(localStorage.getItem("CART")) || [];
 updateCart();
 
 function addToCart(id) {
-    fetch('../data.json')
-  .then((resinicial) => resinicial.json())
-  .then((res) => {
-    const miArray = res;
-    if(cart.some((item) => item.id === id) ){
-        changeNumberOfUnits("plus", id)
-    } 
-    else{
+  fetch("../data.json")
+    .then((resinicial) => resinicial.json())
+    .then((res) => {
+      const miArray = res;
+      if (cart.some((item) => item.id === id)) {
+        changeNumberOfUnits("plus", id);
+      } else {
         const item = miArray.find((product) => product.id === id);
         Toastify({
-            text: 'Agregado al carrito 🧁🍰',
-            duration: 3000,
-            gravity: 'top',
-            position: 'left',
-            style: {
-                background: 'linear-gradient(to right, #E48C89, #4C7295)'
-            }}).showToast();
-        
-        cart.push({
-            ...item,
-            numberOfUnits: 1,
-        })}
-        updateCart();
-    })
+          text: "Agregado al carrito 🧁🍰",
+          duration: 3000,
+          gravity: "top",
+          position: "left",
+          style: {
+            background: "linear-gradient(to right, #E48C89, #4C7295)",
+          },
+        }).showToast();
 
+        cart.push({
+          ...item,
+          numberOfUnits: 1,
+        });
+      }
+      updateCart();
+    });
 }
 
-
 function updateCart() {
-    renderCartItems();
-    renderSubtotal();
+  renderCartItems();
+  renderSubtotal();
 
-    localStorage.setItem("CART", JSON.stringify(cart));
+  localStorage.setItem("CART", JSON.stringify(cart));
 }
 
 function renderSubtotal() {
-    let totalPrice = 0,
-     totalItems =0;
-    cart.forEach((item) => {
-        
-        totalPrice += item.price * item.numberOfUnits;
-        totalItems += item.numberOfUnits;
-    });
-    subtotalEl.innerHTML = `
+  let totalPrice = 0,
+    totalItems = 0;
+  cart.forEach((item) => {
+    totalPrice += item.price * item.numberOfUnits;
+    totalItems += item.numberOfUnits;
+  });
+  subtotalEl.innerHTML = `
     Subtotal (${totalItems} items): $ ${totalPrice}
-    `
+    `;
 }
 
 function renderCartItems() {
-    cartItemsEl.innerHTML = "";
-    cart.forEach((item) => {
-        cartItemsEl.innerHTML += `
+  cartItemsEl.innerHTML = "";
+  cart.forEach((item) => {
+    cartItemsEl.innerHTML += `
         <div class="cart-item">
             <div class="item-info">
                 <img src=${item.img} alt="${item.name}">
@@ -101,97 +98,92 @@ function renderCartItems() {
                 </div>
             </div>
         </div>
-        `
-    })
+        `;
+  });
 }
 
 // removeitems con sweetalert
 
-
-function removeItemFromCart(id){
-const swalWithBootstrapButtons = Swal.mixin({
+function removeItemFromCart(id) {
+  const swalWithBootstrapButtons = Swal.mixin({
     customClass: {
-      confirmButton: 'btn btn-success',
-      cancelButton: 'btn btn-danger'
+      confirmButton: "btn btn-success",
+      cancelButton: "btn btn-danger",
     },
-    buttonsStyling: false
-  })
-  
-  swalWithBootstrapButtons.fire({
-    title: 'Seguro/a que queres eliminar este producto?',
-    text: "Esta accion no puede deshacerse",
-    icon: 'question',
-    showCancelButton: true,
-    confirmButtonText: 'Si, estoy seguro/a!',
-    cancelButtonText: 'No, lo quiero!',
-    reverseButtons: true,
-    showClass: {
-        popup: 'animate__animated animate__fadeInDown'
+    buttonsStyling: false,
+  });
+
+  swalWithBootstrapButtons
+    .fire({
+      title: "Seguro/a que queres eliminar este producto?",
+      text: "Esta accion no puede deshacerse",
+      icon: "question",
+      showCancelButton: true,
+      confirmButtonText: "Si, estoy seguro/a!",
+      cancelButtonText: "No, lo quiero!",
+      reverseButtons: true,
+      showClass: {
+        popup: "animate__animated animate__fadeInDown",
       },
       hideClass: {
-        popup: 'animate__animated animate__fadeOutUp'
+        popup: "animate__animated animate__fadeOutUp",
+      },
+    })
+    .then((result) => {
+      if (result.isConfirmed) {
+        swalWithBootstrapButtons.fire(
+          "Eliminado",
+          "El producto fue eliminado del carrito",
+          "warning"
+        );
+        cart = cart.filter((item) => item.id !== id);
+        updateCart();
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        swalWithBootstrapButtons.fire(
+          "Genial",
+          "Tu producto sigue en el carrito :)",
+          "success"
+        );
       }
-  }).then((result) => {
-    if (result.isConfirmed) {
-      swalWithBootstrapButtons.fire(
-        'Eliminado',
-        'El producto fue eliminado del carrito',
-        'warning',
-      )
-      cart = cart.filter((item) => item.id !== id);
-    updateCart();
-    } else if (
-
-      result.dismiss === Swal.DismissReason.cancel
-    ) {
-      swalWithBootstrapButtons.fire(
-        'Genial',
-        'Tu producto sigue en el carrito :)',
-        'success'
-      )
-    }
-  })
-  
+    });
 }
-
 
 function changeNumberOfUnits(action, id) {
-    cart = cart.map((item) =>{
-        let numberOfUnits = item.numberOfUnits;
-        if(item.id === id) {
-            if(action === "minus" && numberOfUnits > 1){
-                numberOfUnits --;
-            } else if (action === "plus" && numberOfUnits < 5){
-                numberOfUnits++;
-                Toastify({
-                    text: 'Agregado al carrito 🧁🍰',
-                    duration: 3000,
-                    gravity: 'top',
-                    position: 'left',
-                    style: {
-                        background: 'linear-gradient(to right, #E48C89, #4C7295)'
-                    }}).showToast();
-            } else if (action === "plus" && numberOfUnits >= 5){
-                Toastify({
-                    text: 'Limite de 5 unidades ❌',
-                    duration: 3000,
-                    gravity: 'top',
-                    position: 'left',
-                    style: {
-                        background: 'black'
-                    }}).showToast();
-            }
-        }
-        return {
-            ...item,
-            numberOfUnits,
-        };
-    });
-    updateCart();
+  cart = cart.map((item) => {
+    let numberOfUnits = item.numberOfUnits;
+    if (item.id === id) {
+      if (action === "minus" && numberOfUnits > 1) {
+        numberOfUnits--;
+      } else if (action === "plus" && numberOfUnits < 5) {
+        numberOfUnits++;
+        Toastify({
+          text: "Agregado al carrito 🧁🍰",
+          duration: 3000,
+          gravity: "top",
+          position: "left",
+          style: {
+            background: "linear-gradient(to right, #E48C89, #4C7295)",
+          },
+        }).showToast();
+      } else if (action === "plus" && numberOfUnits >= 5) {
+        Toastify({
+          text: "Limite de 5 unidades ❌",
+          duration: 3000,
+          gravity: "top",
+          position: "left",
+          style: {
+            background: "black",
+          },
+        }).showToast();
+      }
+    }
+    return {
+      ...item,
+      numberOfUnits,
+    };
+  });
+  updateCart();
 }
-
-
-
 
 // NEWSLETTER
 
@@ -205,18 +197,18 @@ inputMail = document.querySelector(".newsletterForm");
 salidaNews = document.getElementById("Newsletter");
 
 inputNombre.oninput = () => {
-    nombreForm = inputNombre.value;
-}
+  nombreForm = inputNombre.value;
+};
 inputMail.oninput = () => {
-    emailForm = inputMail.value;
-}
+  emailForm = inputMail.value;
+};
 boton.onclick = (event) => {
-    event.preventDefault();
-    let salidaNewsletter = "Gracias " + nombreForm + " por brindarnos tu mail: " + emailForm;
-    if(inputMail.value != ""){
-        salidaNews.innerHTML = `
+  event.preventDefault();
+  let salidaNewsletter =
+    "Gracias " + nombreForm + " por brindarnos tu mail: " + emailForm;
+  if (inputMail.value != "") {
+    salidaNews.innerHTML = `
         ${salidaNewsletter}
-        ` 
-} 
-}
-
+        `;
+  }
+};
