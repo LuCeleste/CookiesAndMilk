905fea2623b7a5f52114bc2b1ce45518f34790ec
lulceles <lucianaalonso98@gmail.com>
2022-08-09 12:37:08 -0300

    commit separacion js

--- js/newsletter.js
+++ js/newsletter.js
@@ -1,27 +1,25 @@
-
 // NEWSLETTER
 
-
 function newslet(params) {
-    let inputNombre;
-let inputMail;
-let boton;
-let salidaNews;
-boton = document.getElementById("btnNews");
-inputNombre = document.querySelector(".newsletterFormNombre");
-inputMail = document.querySelector(".newsletterForm");
-salidaNews = document.getElementById("Newsletter");
+  let inputNombre;
+  let inputMail;
+  let boton;
+  let salidaNews;
+  boton = document.getElementById("btnNews");
+  inputNombre = document.querySelector(".newsletterFormNombre");
+  inputMail = document.querySelector(".newsletterForm");
+  salidaNews = document.getElementById("Newsletter");
 
-      boton.onclick = (event) => {
-        event.preventDefault();
-        let nombreForm = inputNombre.value;
-        let emailForm = inputMail.value;
-        let salidaNewsletter =
-          "Gracias " + nombreForm + " por brindarnos tu mail: " + emailForm;
-        if (inputMail.value != "") {
-          salidaNews.innerHTML = `
+  boton.onclick = (event) => {
+    event.preventDefault();
+    let nombreForm = inputNombre.value;
+    let emailForm = inputMail.value;
+    let salidaNewsletter =
+      "Gracias " + nombreForm + " por brindarnos tu mail: " + emailForm;
+    if (inputMail.value != "") {
+      salidaNews.innerHTML = `
               ${salidaNewsletter}
               `;
-        }
-      };
+    }
+  };
 }
